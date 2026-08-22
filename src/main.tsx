3e0019d3fdf5a3c8158ce59a3881c66abe223483
alexsha <alexsha.maharjan@stud.th-luebeck.de>
2026-08-22 18:57:06 +0200

    Configure GitHub Pages automated deployment via GitHub Actions

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@/routes";
 import "@/index.css";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
